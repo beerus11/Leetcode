0878fd23f901e469f32c94b004f79eb4decfe5e2
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2022-10-23 00:49:16 +0530

    Time: 269 ms (55.86%), Space: 15.4 MB (35.68%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -2,19 +2,21 @@ from collections import defaultdict
 import heapq
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        memo = collections.defaultdict(list)
-        for i,j,c in flights:
-            memo[i].append((j,c))
-        q = [(0,0,src)]
+        graph = collections.defaultdict(list)
+        for s, d, c in flights:
+            graph[s].append((d, c))
+ 
+        pq = [(0, src, 0)] 
         seen = {}
-        while q:
-            cost, stop, cur = heapq.heappop(q)
-            if cur == dst:
-                return cost
-            if cur not in seen or stop < seen[cur]:
-                seen[cur] = stop
-                if stop <= k:
-                    for other, price in memo[cur]:
-                        if other not in seen or stop + 1 < seen[other]:
-                            heapq.heappush(q, (price + cost, stop + 1, other))
+ 
+        while pq:
+            curr_cost, curr_node, curr_stops = heapq.heappop(pq)
+            if curr_node == dst:
+                return curr_cost
+            if curr_node not in seen or curr_stops<seen[curr_node]:
+                seen[curr_node]=curr_stops
+                if curr_stops <= k:
+                    for dest, cost in graph[curr_node]:
+                        if dest not in seen or curr_stops+1<seen[dest]:
+                            heapq.heappush(pq, (curr_cost + cost, dest, curr_stops+1))
         return -1
