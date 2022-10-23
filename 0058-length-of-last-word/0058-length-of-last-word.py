2ecba161b5745127cb811778aecd899f65bc8907
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2022-10-23 21:41:12 +0530

    Time: 30 ms (48.38%), Space: 13.9 MB (6.71%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s.strip()
+        return len(s.split(" ")[-1])
+        
