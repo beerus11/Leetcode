285e59caa41ffdf28c79b890cf9ff6dead036a19
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2022-10-28 23:16:11 +0530

    Time: 76 ms (55.15%), Space: 14.2 MB (23.57%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -2,13 +2,13 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         self.ans = []
         def p(arr,i):
-            if i == len(arr):
+            if i == len(arr)-1:
+                self.ans.append(arr[:])
                 return
-            self.ans.append(tuple(arr[:]))
             for j in range(i,len(arr)):
                 arr[i],arr[j] = arr[j],arr[i]
                 p(arr,i+1)
                 arr[i],arr[j] = arr[j],arr[i]
                 
         p(nums,0)
-        return set(self.ans)
+        return self.ans
