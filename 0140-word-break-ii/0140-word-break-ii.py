adee49a10eebdeb84898a4f69df3103d4739ceb2
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2022-10-28 19:47:27 +0530

    Time: 57 ms (43.47%), Space: 13.9 MB (81.46%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -4,14 +4,11 @@ class Solution:
         def wb(word,i,w):
             if i == len(word):
                 self.ans.append(" ".join(w))
-                return True
             for j in range(i,len(word)+1):
                 if word[i:j] in wordDict:
                     w.append(word[i:j])
                     wb(word,j,w)
                     w.pop()
-                
-            return False
         wb(s,0,[])
         return self.ans
         
