dfab0bef60d96e92bafc6afcbfda6285e4fd7e10
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2022-10-28 22:49:15 +0530

    Time: 186 ms (25.18%), Space: 23.3 MB (7.01%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,14 +1,19 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         self.ans = []
+        self.dp = {}
         def cs(candidates,i,s,arr):
-            #print(i,s,arr)
+            if (i,s,tuple(arr)) in self.dp:
+                return
             if s == 0:
                 self.ans.append(tuple(arr[:]))
+                self.dp[(i,s,tuple(arr))]=True
                 return
             if i == len(candidates) or s<0:
+                self.dp[(i,s,tuple(arr))]=True
                 return
             if candidates[i]>target:
+                self.dp[(i,s,tuple(arr))]=True
                 return cs(candidates,i+1,s,arr)
             
             arr.append(candidates[i])
@@ -20,6 +25,8 @@ class Solution:
             arr.pop()
             
             cs(candidates,i+1,s,arr)
+            
+            self.dp[(i,s,tuple(arr))]=True
         cs(candidates,0,target,[])
         return set(self.ans)
         
