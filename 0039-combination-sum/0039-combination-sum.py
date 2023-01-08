81209b9016840f9c1c2c39e9322f42d92bba6a4e
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-01-08 20:35:43 +0530

    Time: 66 ms (81.58%), Space: 14 MB (64.81%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,31 +1,15 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         self.ans = []
-        self.dp = {}
-        def cs(candidates,i,s,arr):
-            if (i,s,tuple(arr)) in self.dp:
+        def cs(candidates,t,path):
+            if t < 0:
+                return 
+            if t ==0 :
+                self.ans.append(path)
                 return
-            self.dp[(i,s,tuple(arr))]=True
-            #print(i,s,arr)
-            if s == 0:
-                self.ans.append(tuple(arr[:]))
-                return
-            if i == len(candidates) or s<0:
-                return
-            if candidates[i]>target:
-                return cs(candidates,i+1,s,arr)
-            
-            arr.append(candidates[i])
-            cs(candidates,i+1,s-candidates[i],arr)
-            arr.pop()
-            
-            arr.append(candidates[i])
-            cs(candidates,i,s-candidates[i],arr)
-            arr.pop()
-            
-            cs(candidates,i+1,s,arr)
-            
-            self.dp[(i,s,tuple(arr))]=True
-        cs(candidates,0,target,[])
-        return set(self.ans)
+            for i in range(len(candidates)):
+                cs(candidates[i:],t-candidates[i],path+[candidates[i]])
+           
+        cs(candidates,target,[])
+        return self.ans
         
