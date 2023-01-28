5411faa56b9727a9768c61f911a7d7c348de36af
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-01-28 10:26:45 +0530

    Time: 28 ms (98.94%), Space: 14.2 MB (15.02%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,14 +1,19 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         wd = set(wordDict)
-        @lru_cache
+        dp = {}
         def wb(i,s):
+            if i in dp:
+                return dp[i]
             if i==len(s):
-                return True
+                dp[i]= True
+                return dp[i]
             for j in range(i+1,len(s)+1):
                 #print(s[i:j],s[i:j] in wd)
                 if s[i:j] in wd and wb(j,s):
-                    return True
-            return False
+                    dp[i]= True
+                    return dp[i]
+            dp[i] = False
+            return dp[i]
                     
         return wb(0,s)
