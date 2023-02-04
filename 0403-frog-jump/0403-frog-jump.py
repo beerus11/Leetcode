eb1398316d27348128b62cfb4f2812809f1494bc
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-02-04 17:21:50 +0530

    Time: 282 ms (53.29%), Space: 15.8 MB (96.29%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.py
+++ 0403-frog-jump/0403-frog-jump.py
@@ -3,13 +3,14 @@ class Solution:
         hm = {}
         for i in range(len(stones)):
             hm[stones[i]]=set()
-        hm[0].add(0)
+        hm[stones[0]].add(0)
         
         for i in range(len(stones)):
             for k in hm[stones[i]]:
                 for s in [k-1,k,k+1]:
                     if s>0 and stones[i]+s in hm:
                         hm[stones[i]+s].add(s)
+        #print(hm)
         return len(hm[stones[len(stones)-1]])>0
             
         
