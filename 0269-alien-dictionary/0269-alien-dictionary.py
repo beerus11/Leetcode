06a1017bf47fd0ea7794b9e9ea24f41b7c98a7d1
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-02-04 11:45:04 +0530

    Time: 39 ms (59.82%), Space: 13.8 MB (99.87%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.py
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -1,36 +1,40 @@
-from collections import defaultdict
-class Solution(object):
-    def alienOrder(self, words):
-        """
-        :type words: List[str]
-        :rtype: str
-        """
-        g = defaultdict(list)
+from collections import defaultdict,Counter
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
         
-        indegree = {c:0 for word in words for c in word}
-        for fw,sw in zip(words,words[1:]):
-            for a,b in zip(fw,sw):
+        indegree = Counter({c : 0 for word in words for c in word})
+        g = defaultdict(list)
+        for i in range(len(words)-1):
+            for a,b in list(zip(words[i],words[i+1])):
                 if a!=b:
-                    if b not in g[a]:
-                        g[a].append(b)
-                        indegree[b]+=1
+                    g[a].append(b)
+                    indegree[b]+=1
                     break
             else:
-                if len(sw)<len(fw): return ""
-        output = []
-        q = [a for a in indegree if indegree[a]==0]
-        
-        while q:
-            c = q.pop(0)
-            output.append(c)
-            for i in g[c]:
-                indegree[i]-=1
-                if indegree[i]==0:
-                    q.append(i)
-        if len(output)<len(indegree):
-            return ""
-        return "".join(output)
-                    
+                if len(words[i+1]) < len(words[i]): return ""
                 
+                    
+        q = []
+        for k,v in indegree.items():
+            if v ==0:
+                q.append(k)
+                
+        if len(q)==0 and len(indegree)==1:
+            return words[0][0]
+
+        order = []
+        while q:
+            n = q.pop(0)
+            order.append(n)
+            for nei in g[n]:
+                indegree[nei]-=1
+                if indegree[nei]==0:
+                    q.append(nei)
+        if len(indegree)> len(order):
+            return ""
+                    
+        return "".join(order)
+                
+        
             
         
