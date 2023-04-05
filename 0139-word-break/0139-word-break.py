bdec873deac01df9224dd44fb09ad4f4c2cec2dd
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-04-05 12:51:05 +0530

    Time: 48 ms (23.84%), Space: 14.1 MB (23.28%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,19 +1,21 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        wd = set(wordDict)
-        dp = {}
-        def wb(i,s):
-            if i in dp:
-                return dp[i]
-            if i==len(s):
-                dp[i]= True
-                return dp[i]
-            for j in range(i+1,len(s)+1):
-                #print(s[i:j],s[i:j] in wd)
-                if s[i:j] in wd and wb(j,s):
-                    dp[i]= True
-                    return dp[i]
-            dp[i] = False
-            return dp[i]
-                    
-        return wb(0,s)
+        self.dp = {}
+        def wb(w,i,j):
+            if (i,j) in self.dp:
+                return self.dp[(i,j)]
+            if len(w)==0:
+                self.dp[(i,j)] = True
+                return self.dp[(i,j)]
+            if w[i:j] in wordDict:
+                self.dp[(i,j)] = True
+                return self.dp[(i,j)]
+            for k in range(i,j):
+                if w[i:k+1] in wordDict and wb(w,k+1,j):
+                    self.dp[(i,j)] = True
+                    return self.dp[(i,j)]
+            self.dp[(i,j)] = False
+            return self.dp[(i,j)]
+        return wb(s,0,len(s))
+                
+        
