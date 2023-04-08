a18c521ef7976a0dbcc33acb23555039974aa448
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-04-08 22:25:52 +0530

    Time: 501 ms (87.65%), Space: 26 MB (30.41%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,34 +1,33 @@
-from collections import defaultdict
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        # if no of nodes less than or equal to 2
         if n <=2:
             return [i for i in range(n)]
         
-        graph = defaultdict(set)
+        g = defaultdict(set)
         
-        for u,v in edges:
-            graph[u].add(v)
-            graph[v].add(u)
+        for u, v in edges:
+            g[u].add(v)
+            g[v].add(u)
+            
         leaves = []
         for i in range(n):
-            if len(graph[i])==1:
+            if len(g[i])==1:
                 leaves.append(i)
                 
-        reamining_leaves = n    
-        while reamining_leaves>2:
-            reamining_leaves -= len(leaves)
+        remaining_leaves = n
+        
+        while remaining_leaves >2:
+            remaining_leaves -= len(leaves)
             new_leaves = []
             while leaves:
-                leave = leaves.pop()
-                
-                nei = graph[leave].pop()
-                graph[nei].remove(leave)
-                
-                if len(graph[nei]) == 1:
+                leaf = leaves.pop()
+                nei = g[leaf].pop()
+                g[nei].remove(leaf)
+                if len(g[nei])==1:
                     new_leaves.append(nei)
-                
             leaves = new_leaves
-                
+            
         return leaves
-        
+                    
+            
+            
