8d33f61be565f06ef87433d05ecd20a0a6afa103
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-04-21 23:13:47 +0530

    Time: 30 ms (73.35%), Space: 13.8 MB (68.34%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -1,14 +1,15 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         self.ans = []
-        def wb(word,i,w):
-            if i == len(word):
-                self.ans.append(" ".join(w))
+        def wb(word,arr,i):
+            if i== len(word):
+                self.ans.append(" ".join(arr))
+                return
             for j in range(i,len(word)+1):
                 if word[i:j] in wordDict:
-                    w.append(word[i:j])
-                    status = wb(word,j,w)                       
-                    w.pop()
-        wb(s,0,[])
+                    arr.append(word[i:j])
+                    wb(word,arr[:],j)
+                    arr.pop()
+        wb(s,[],0)
         return self.ans
         
