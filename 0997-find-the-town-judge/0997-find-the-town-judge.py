b904723b82908e713861fb0bac5ce25d48bd4c7a
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-04-22 10:16:50 +0530

    Time: 742 ms (52.35%), Space: 19 MB (20.31%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,22 +1,26 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         if len(trust)==0 and n==1:
-            return n
-        if len(trust)==0 and n>1:
+            return 1
+        if len(trust)==0 and n>-1:
             return -1
-        indegree = [0]*(n+1)
-        outdegree = [0]*(n+1)
+        
+        
+        g = defaultdict(list)
+        
+        indegree = {i:0 for i in range(1,n+1)}
+        outdegree = {i:0 for i in range(1,n+1)}
+        
         for a,b in trust:
             indegree[b]+=1
             outdegree[a]+=1
-        print(indegree)
-        print(outdegree)
-        ans = []
-        for k,i in enumerate(indegree):
-            if i == (n-1) and outdegree[k]==0:
-                ans.append(k)    
-        print(ans)
-        return ans[0] if len(ans)==1 else -1
+        
+        arr = []
+        for k,v in outdegree.items():
+            if v==0 and indegree[k]==n-1:
+                arr.append(k)
+        
+        return arr[0] if len(arr)==1 else -1
             
-            
+                
         
