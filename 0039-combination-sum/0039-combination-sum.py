a4e89091e9754c9120593d197c3fcb7699d854a8
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-04-22 21:37:55 +0530

    Time: 54 ms (84.00%), Space: 13.9 MB (89.04%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,15 +1,17 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        self.ans = []
-        def cs(candidates,t,path):
-            if t < 0:
-                return 
-            if t ==0 :
-                self.ans.append(path)
+        ans = []
+        def cs(arr,s,i):
+            if s==target:
+                ans.append(arr[:])
                 return
-            for i in range(len(candidates)):
-                cs(candidates[i:],t-candidates[i],path+[candidates[i]])
-           
-        cs(candidates,target,[])
-        return self.ans
+            if s>target:
+                return
+            for j in range(i,len(candidates)):
+                arr.append(candidates[j])
+                cs(arr,s+candidates[j],j)
+                arr.pop()
+        cs([],0,0)
+        return ans
+                
         
