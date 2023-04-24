4c410779e43346960a96841a23e24323ce458cb7
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-04-24 08:27:16 +0530

    Time: 646 ms (50.84%), Space: 14.2 MB (93.22%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,28 +1,25 @@
+from collections import deque
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        if m == 1 and n == 1:
-            return 1 if not grid[0][0] else -1
-        
-        if grid[0][0] or grid[m - 1][n - 1]:
+        if grid[0][0]==1:
             return -1
-        
-        directions = ((1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1))
-        q = collections.deque([(0, 0, 1)])
-        
+        if grid[len(grid)-1][len(grid[0])-1]==1:
+            return -1
+        if len(grid)==1 and len(grid[0])==1:
+            return 1 if grid[0][0]==0 else -1
+        q = deque([(0,0,1)])
+        dirs = [(1,0),(0,1),(-1,0),(0,-1),(-1,-1),(-1,1),(1,-1),(1,1)]
         while q:
-            i, j, pathLength = q.popleft()
-            for di, dj in directions:
-                newI, newJ = i + di, j + dj
-                if newI == m - 1 and newJ == n - 1:
-                    return pathLength + 1
-                
-                if newI == -1 or newI == m or newJ == -1 or newJ == n or grid[newI][newJ]:
+            i,j,l = q.popleft()
+            for a,b in dirs:
+                x,y=i+a,j+b
+                if x==len(grid)-1 and y==len(grid[0])-1:
+                    return l+1
+                if x == -1 or x == len(grid) or y == -1 or y == len(grid[0]) or grid[x][y]:
                     continue
-                
-                grid[newI][newJ] = 1
-                q.append((newI, newJ, pathLength + 1))
-        
+                grid[x][y] = 1
+                q.append((x, y, l + 1))
         return -1
+        
                 
         
