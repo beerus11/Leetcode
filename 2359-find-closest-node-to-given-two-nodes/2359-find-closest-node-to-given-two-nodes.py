ba17497f5546f6ea8921137831e4f690e2a00410
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-04-28 20:18:34 +0530

    Time: 2016 ms (5.16%), Space: 164.2 MB (5.16%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
@@ -1,19 +1,33 @@
 class Solution:
     def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
-        def dfs(node):
-            d = {node:0}
-            while node != -1:
-                next_node = edges[node]
-                if next_node in d:
-                    return d
-                d[next_node] = d[node] + 1
-                node = next_node
-            return d
-        x = dfs(node1)
-        y = dfs(node2)
+        self.d = {}
+        l = len(edges)
+        g = defaultdict(list)
+        for a,b in enumerate(edges):
+            if b!=-1:
+                g[a].append(b)
+            
+        def dfs(n,d):
+            if n==None:
+                return
+            self.d[n]=d
+            for nei in g[n]:
+                if (nei not in self.d) or d+1< self.d[nei]:
+                    dfs(nei,d+1)
+        d1,d2 = {},{}
+        dfs(node1,0)
+        d1=self.d
+        self.d = {}
+        dfs(node2,0)
+        d2=self.d
         
-        dc = ((max(x[node], y[node]), node) for node in x.keys() & y.keys())
-        return min(dc, default=(0, -1))[1]
-            
-            
+        arr = []
+        for k,v in enumerate(edges):
+            if k in d1 and k in d2:
+                arr.append((max(d1[k],d2[k]),k))
+        arr = sorted(arr,key= lambda x:x[0])
+        if len(arr)==0:
+            return -1
+        print(arr)
+        return arr[0][1]
         
