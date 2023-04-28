66f8d2caa16537784629d2579b52bc99ed8c3e90
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-04-28 20:32:11 +0530

    Time: 902 ms (12.37%), Space: 55.1 MB (5.29%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -1,11 +1,14 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        count_map = collections.Counter([i for e in edges for i in e])
-        mx = -1*float('inf')
-        ans = None
-        for k,v in count_map.items():
-            mx = max(mx,v)
-            if mx == v:
-                ans = k
-        return ans
-        
+        degree = defaultdict(int)
+        s = set() 
+        for a,b in edges:
+            degree[a]+=1
+            degree[b]+=1
+            s.add(a)
+            s.add(b)
+        l = len(s)
+        for k,v in degree.items():
+            if v==l-1:
+                return k
+        return -1
