701b36b554752a7679e5b727eb8194c0ee57b89c
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-04-30 17:36:37 +0530

    Time: 91 ms (31.69%), Space: 16.9 MB (5.99%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,16 +1,14 @@
-from collections import defaultdict
-import heapq
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         g = defaultdict(list)
+        
         for a,b in tickets:
             insort(g[a],b)
         path = []
-        def dfs(n):
-            while g[n]:
-                dfs(g[n].pop(0))  
-            path.append(n)
+        def dfs(node):
+            while g[node]:
+                dfs(g[node].pop(0))
+            path.append(node)
         dfs("JFK")
         return path[::-1]
-
-            
+        
