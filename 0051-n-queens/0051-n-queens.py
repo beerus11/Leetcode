f63f54bf083683023bedecb9f2fb4e7d7c8fa294
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-04-30 17:19:40 +0530

    Time: 149 ms (12.72%), Space: 16.9 MB (5.01%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -9,19 +9,17 @@ class Solution:
                 if r - i >= 0 and c + i < n and board[r-i][c+i] == 'Q':
                     return False
             return True
-        ans = []
-        def solve(board,i):
-            if i ==len(board):
+        ans =[]
+        def backtrack(i,board):
+            if i==n:
                 ans.append(["".join(b) for b in board])
-                return
+                return 
             for j in range(n):
                 if isSafe(board,i,j):
-                    board[i][j]='Q'
-                    solve(board,i+1)
-                    board[i][j]='.'
-                     
-        board = [['.']*n for i in range(n)]
-        solve(board,0)
-        
+                    board[i][j]="Q"
+                    backtrack(i+1,board)
+                    board[i][j]="."
+                
+        b = [['.']*n for i in range(n)]       
+        backtrack(0,b)
         return ans
-        
