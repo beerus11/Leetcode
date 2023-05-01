85f8d62425d0af12888aa6a4637385c9bb69c725
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-05-01 09:47:49 +0530

    Time: 55 ms (5.13%), Space: 16.3 MB (5.23%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,15 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         @lru_cache(None)
-        def mxa(i,x):
+        def mxa(i):
             if i>=len(nums):
                 return 0
-            if i-1==x:
-                return mxa(i+1,x)
-            else:
-                a = nums[i]+mxa(i+2,i)
-                b = mxa(i+1,x)
-                return max(a,b)
-        return mxa(0,-2)
+            a = nums[i]+mxa(i+2)
+            b = mxa(i+1)
+            return max(a,b)
+        return mxa(0)
             
         
