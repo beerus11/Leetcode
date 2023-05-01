875b63fef969fdb557be7123314a12c32ccfd6ea
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-05-01 19:47:58 +0530

    Time: 44 ms (7.17%), Space: 16.2 MB (5.03%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -1,29 +1,28 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def removeNthFromEnd(self, head, n):
-        """
-        :type head: ListNode
-        :type n: int
-        :rtype: ListNode
-        """
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         hm = {}
-        i = 1
-        temp = head
+        temp = ListNode(-1)
+        temp.next = head
+        ans = temp
+        i =0
         while temp:
             hm[i]=temp
             temp = temp.next
             i+=1
-        x = i-n
-        print(x)
-        if x-1 not in hm:
-            return head.next
-        prev = hm[x-1]
-        print(prev.val)
-        prev.next = hm[x+1] if x+1 in hm else None
-        
-        return head
+        if n==1:
+            x = i-n
+            hm[x-1].next = None
+        else:
+            j = i-n
+            p = hm[j-1]
+            n = hm[j+1] if j+1<i else None
+            p.next = n
+        return ans.next
+            
+            
         
