2f7b700a8386bf55697f683179f0aef6f31051ef
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-05-02 22:14:46 +0530

    Time: 503 ms (20.14%), Space: 18.7 MB (5.62%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,23 +1,25 @@
-from collections import defaultdict
-import heapq
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        g = defaultdict(dict)
+        g = defaultdict(defaultdict)
         
-        for s,d,t in times:
-            g[s][d]=t
-        signalRcvd = [float('inf')]*(n+1)
-        signalRcvd[0]=0
-        signalRcvd[k]=0
+        for a,b,c in times:
+            g[a][b] = c
+            
         q = [(k,0)]
+        m = {k:0}
         while q:
-            n,t = heapq.heappop(q)
-            if t> signalRcvd[n]:
-                continue
-            for nei,v in g[n].items():
-                if signalRcvd[nei]>t+v:
-                    signalRcvd[nei] = t+v
-                    heapq.heappush(q,(nei,t+v))
-        mx = max(signalRcvd)
-        print(signalRcvd)
-        return mx if mx!=float('inf') else -1
+            node,t = q.pop(0)
+            for nei,v in g[node].items():
+                x = v+t
+                if (nei not in m) or x < m[nei]:
+                    m[nei]=x
+                    q.append((nei,x))
+        #print(m)
+        if len(m)<n:
+            return -1
+        return max([v for k,v in m.items()])
+                
+        
+            
+        
+        
