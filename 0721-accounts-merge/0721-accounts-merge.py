9bbc1ab8f0c191c6e7d36e7cbeb282f289f50fe7
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-05-07 20:56:40 +0530

    Time: 226 ms (35.19%), Space: 25.9 MB (9.85%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,28 +1,27 @@
-from collections import defaultdict
 class Solution:
-    def dfs(self, graph, node, visit):
-        visit.add(node)
-        for nei in graph[node]:
-            if nei not in visit:
-                self.dfs(graph, nei, visit)
-        self.res.append(node)
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        graph = collections.defaultdict(set)
+            
+        g = defaultdict(list)
         for account in accounts:
             for email in account[1:]:
-                graph[account[1]].add(email)
-                graph[email].add(account[1])
-                
+                g[email].append(account[1])
+                g[account[1]].append(email)
         visit = set()
         ans = []
+        res = []
+        
+        def dfs(node):
+            visit.add(node)
+            for nei in g[node]:
+                if nei not in visit:
+                    dfs(nei)
+            res.append(node)
+            
         for account in accounts:
             name = account[0]
             for email in account[1:]:
                 if email not in visit:
-                    self.res = []
-                    self.dfs(graph, email, visit)
-                    ans.append([name]+sorted(self.res))
+                    res = []
+                    dfs(email)
+                    ans.append([name]+sorted(res))
         return ans
-            
-            
-        
