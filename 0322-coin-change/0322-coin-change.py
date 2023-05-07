218a6c7fb64a3e8d39a01f4f9632de4d980291bd
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-05-07 09:47:00 +0530

    Time: 2416 ms (6.60%), Space: 590.8 MB (5.06%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,24 +1,18 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        coins.sort(reverse=True)
-        self.dp ={}
-        def cc(coins,amt,i):
-            if i==len(coins):
-                return -1
-            if amt ==0:
+        @lru_cache(None)
+        def cc(i,t):
+            if t==0:
                 return 0
-            if (i,amt) in self.dp:
-                return self.dp[(i,amt)]
-            if coins[i]>amt:
-                self.dp[(i,amt)]= cc(coins,amt,i+1)
-                return self.dp[(i,amt)]
-            a = cc(coins,amt-coins[i],i)
-            b = cc(coins,amt,i+1)
-            if a==-1 and b==-1:
-                self.dp[(i,amt)] =-1
-            elif a==-1 or b==-1:
-                self.dp[(i,amt)]= max(1+a,b)
-            else:
-                self.dp[(i,amt)] = min(1+a,b)
-            return self.dp[(i,amt)]
-        return cc(coins,amount,0)
+            if i<0 or t<0:
+                return float('inf')
+            if coins[i]>t:
+                return cc(i-1,t)
+            a = 1+ cc(i,t-coins[i])
+            b = cc(i-1,t)
+            return min(a,b)
+        a = cc(len(coins)-1,amount)
+        return -1 if a==float('inf') else a
+            
+            
+        
