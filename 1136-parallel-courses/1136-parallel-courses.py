2dd42d19791bc17a20df9b0456dae4d74998282c
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-05-07 07:02:47 +0530

    Time: 277 ms (40.96%), Space: 20.1 MB (14.42%) - LeetHub

--- 1136-parallel-courses/1136-parallel-courses.py
+++ 1136-parallel-courses/1136-parallel-courses.py
@@ -1,17 +1,17 @@
-from collections import defaultdict
 class Solution:
     def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
-        g = defaultdict(list)
+        g= defaultdict(list)
         indegree = {i:0 for i in range(1,n+1)}
         for a,b in relations:
             g[a].append(b)
             indegree[b]+=1
+            
         q = []
         for k,v in indegree.items():
             if v==0:
                 q.append(k)
-        count = 0
-        steps = 0
+        steps,count=0,0
+        
         while q:
             steps+=1
             nq = []
@@ -21,8 +21,8 @@ class Solution:
                     indegree[nei]-=1
                     if indegree[nei]==0:
                         nq.append(nei)
-            q  = nq
-
-        return steps if count == n else -1
-                
+            q = nq
+        return steps if count==n else -1
+            
+        
         
