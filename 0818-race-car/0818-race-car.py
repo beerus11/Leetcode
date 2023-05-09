958ce7af504f99c134005cc51e3af4296835b1b9
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-05-09 06:44:06 +0530

    Time: 59 ms (55.51%), Space: 18.4 MB (36.02%) - LeetHub

--- 0818-race-car/0818-race-car.py
+++ 0818-race-car/0818-race-car.py
@@ -2,22 +2,19 @@ class Solution:
     def racecar(self, target: int) -> int:
         q = [(0,0,1)]
         visited = set()
+        
         while q:
-            moves,pos,speed = q.pop(0)
-            
-            if pos == target:
-                return moves
-            
-            if (pos,speed) in visited:
+            m,p,s = q.pop(0)
+            if p==target:
+                return m
+            if (p,s) in visited:
                 continue
             else:
-                visited.add((pos,speed))
-                
-            q.append((moves+1,pos+speed,speed*2))
+                visited.add((p,s))
+                q.append((m+1,p+s,s*2))
             
-            if (pos+speed>target and speed>0) or (pos+speed<target and speed<0):
-                speed = -1 if speed>0 else 1
-                q.append((moves+1,pos,speed))
-        return -1
+                if (p+s>target and s>0) or (p+s<target and s<0):
+                    s = -1 if s>0 else 1
+                    q.append((m+1,p,s))
                 
-            
+        
