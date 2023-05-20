a1753393e65e1f87d3f76c53de045235fd37ad2b
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-05-20 16:12:36 +0530

    Time: 46 ms (64.08%), Space: 16.7 MB (8.05%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,20 +1,18 @@
 class Solution:
-    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+    def uniquePathsWithObstacles(self, g: List[List[int]]) -> int:
+        if g[0][0]==1 or g[len(g)-1][len(g[0])-1]==1:
+            return 0
         self.dp = {}
-        def up(grid,i,j):
-            if i>=len(grid) or j>=len(grid[0]):
-                return 0
+        def up(i,j):
             if (i,j) in self.dp:
                 return self.dp[(i,j)]
-            if grid[i][j]==1:
-                self.dp[(i,j)] = 0
-                return self.dp[(i,j)]
-            if i==len(grid)-1 and j==len(grid[0])-1:
-                self.dp[(i,j)] = 1
-                return self.dp[(i,j)]
-            a = up(grid,i+1,j)
-            b = up(grid,i,j+1)
-            self.dp[(i,j)] =  a+b
+            if i==len(g) or j ==len(g[0]):
+                return 0
+            if i==len(g)-1 and j==len(g[0])-1:
+                return 1
+            if g[i][j]==1:
+                return 0
+            self.dp[(i,j)]=up(i+1,j) + up(i,j+1)
             return self.dp[(i,j)]
-        return up(obstacleGrid,0,0)
+        return up(0,0)
         
