c438afaa6365758bbe2c1faf1df395fcd355a6cc
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-05-21 10:13:33 +0530

    Time: 100 ms (19.97%), Space: 16.8 MB (23.54%) - LeetHub

--- 1366-rank-teams-by-votes/1366-rank-teams-by-votes.py
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.py
@@ -1,5 +1,16 @@
+from functools import cmp_to_key
 class Solution:
     def rankTeams(self, votes: List[str]) -> str:
+        def sort_rank(a,b):
+            x,y = a[1],b[1]
+            i=0
+            while i<len(x):
+                if x[i]>y[i]:
+                    return -1
+                elif x[i]<y[i]:
+                    return 1
+                i+=1
+            return -1 if ord(a[0])<ord(b[0]) else 1
         hashMap = {}
         for vote in votes:
             for indx, char in enumerate(vote):
@@ -8,8 +19,8 @@ class Solution:
                 else:
                     hashMap[char] = [0] * len(vote)
                     hashMap[char][indx] += 1
-        result = sorted(hashMap.keys(), key = lambda x: (hashMap[x],-1*ord(x)), reverse=True)
-        
+        result = sorted(hashMap.items(), key = cmp_to_key(sort_rank))
+        result = [r[0] for r in result]
         return "".join(result)
                 
             
