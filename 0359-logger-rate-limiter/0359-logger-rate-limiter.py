783d381b679e6c7a98256fa099a81d7e32e74bb8
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-06-04 21:35:09 +0530

    Time: 190 ms (5.42%), Space: 25.9 MB (12.63%) - LeetHub

---
+++ 0359-logger-rate-limiter/0359-logger-rate-limiter.py
@@ -0,0 +1,25 @@
+class Logger:
+
+    def __init__(self):
+        self.logs = defaultdict(set)
+        
+
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        ans = []
+        if timestamp in self.logs and message in self.logs[timestamp]:
+            print(self.logs)
+            return False
+        start = 0 if timestamp-9<0 else timestamp-9
+        for i in range(start,timestamp):
+            if message == "foo" and timestamp==11:
+                print(i,self.logs[i])
+            if i in self.logs and message in self.logs[i] :
+                return False
+        self.logs[timestamp].add(message)
+        return True
+        
+
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
