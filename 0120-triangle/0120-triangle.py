9636febbd2140a61761d352a3e8c16e850708a3b
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-06-24 18:16:47 +0530

    Time: 101 ms (5.32%), Space: 20.2 MB (6.47%) - LeetHub

---
+++ 0120-triangle/0120-triangle.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        @lru_cache(None)
+        def mnp(i,j):
+            if i == len(triangle)-1 and j <=len( triangle[i])-1:
+                return triangle[i][j]
+            if i == len(triangle)-1 and j >len( triangle[i])-1:
+                return float('inf')
+            print(j,j+1)
+            a = mnp(i+1,j)
+            b = mnp(i+1,j+1)
+            return triangle[i][j]+min(a,b)
+        return mnp(0,0)
+        
+            
+        
