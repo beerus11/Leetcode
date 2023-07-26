0d07b18eaaf529718fb939abcfd22c113d49266f
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-07-26 09:02:25 +0530

    Time: 55 ms (20.80%), Space: 16.3 MB (70.82%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,21 +1,19 @@
-class Solution(object):
-    def reverse(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
-        if x==0:
-            return 0
-        isNeg = x<0
-        x = abs(x)
-        ans = []
-        while x>0:
-            ans.append(str(x%10))
-            x = x//10
-        x = int("".join(ans))
-        if x>2**31:
-            return 0
-        if isNeg:
-            return -1*x
-        return int(x)
+class Solution:
+    def reverse(self, x: int) -> int:
+        def rev(no):
+            arr = []
+            while no>0:
+                arr.append(no%10)
+                no=no//10
+            no =0
+            while len(arr)>0:
+                no = no*10+arr.pop(0)   
+            
+            return no
+        z = 2**31
+        if x>=0:
+            y = rev(x)
+            return y if -1*z <y<z-1 else 0
+        y = -1*rev(-1*x)
+        return y if -1*z <y<z-1 else 0
         
