9395db0ff1c61d5f79e63fe3cd4c5099ab6f10c7
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-08-20 18:18:06 +0530

    Time: 37 ms (82.53%), Space: 16.4 MB (12.33%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -1,16 +1,18 @@
 class Solution:
-    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
-        q = [(start,0)]
-        seen = {start}
+    def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
+        q = [(startGene,0)]
+        seen = set()
+        bs = set(bank)
         while q:
-            n,steps = q.pop(0)
-            if n == end:
-                return steps
+            n,s = q.pop(0)
+            if n == endGene:
+                return s
             for c in "ACGT":
                 for i in range(len(n)):
                     nei = n[:i]+c+n[i+1:]
-                    if nei not in seen and nei in bank:
-                        q.append((nei,steps+1))
+                    if nei not in seen and nei in bs:
+                        q.append((nei,s+1))
                         seen.add(nei)
         return -1
+            
         
