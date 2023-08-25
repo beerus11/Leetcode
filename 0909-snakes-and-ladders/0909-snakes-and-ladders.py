9ea0ecfd2eefa0b9ea6b2ecfe7e34912e20c9265
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-08-25 13:21:00 +0530

    Time: 105 ms (91.02%), Space: 16.2 MB (98.32%) - LeetHub

---
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -0,0 +1,31 @@
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        board = board[::-1]
+        n = len(board)
+        cells = [None] * (n**2+1)
+        
+        index = 1
+        for row in range(n):
+            if row%2==0:
+                for column in range(n):
+                    cells[index] = board[row][column]
+                    index+=1
+            else:
+                for column in range(n-1,-1,-1):
+                    cells[index] = board[row][column]
+                    index+=1
+        q = [(1,0)]
+        m = {1:0}
+        while q:
+            node,steps = q.pop(0)
+            if cells[node]!=-1:
+                node = cells[node]
+            if node == n**2:
+                return steps
+            for i in range(node+1,node+7):
+                if i not in m:
+                    m[i]=steps+1
+                    q.append((i,steps+1))
+        return -1
+        
+        
