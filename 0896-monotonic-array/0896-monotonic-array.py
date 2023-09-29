78b07a112c31e2f56733c50bb23c2ba8ecc50c36
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-09-29 06:49:47 +0530

    Time: 833 ms (58.10%), Space: 30.4 MB (83.11%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.py
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -6,12 +6,10 @@ class Solution:
         pv = 0
         for i in range(1,len(nums)):
             x = nums[i]-nums[i-1]
-            print(x)
             if pv ==0 or x==0 or pv*x>0:
                 if x!=0:
                     pv = x
             else:
-                print(pv,x)
                 return False
         return True
             
