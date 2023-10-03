529e404030f976e92cbe35390fb96a3d42d09070
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-10-03 15:59:21 +0530

    Time: 36 ms (71.66%), Space: 16.3 MB (26.45%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,32 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        dst = []
+        cst = []
+        i = 0
+        while i < len(s):
+            #print(dst,cst)
+            ch = s[i]
+            no = 0
+            while s[i].isdigit():
+                no=no*10+ int(s[i])
+                i+=1
+                print(no)
+            if no>0:   
+                dst.append(no)
+                i-=1
+            elif (ch>='a' and ch<='z') or ch =="[":
+                cst.append(ch)
+            elif ch=="]":
+                arr = []
+                while cst and cst[-1]!="[":
+                    arr.append(cst.pop())
+                if cst:
+                    cst.pop()
+                if dst:
+                    cst.append("".join(arr[::-1])*dst.pop())
+                else:
+                    cst.append("".join(arr[::-1]))
+            i+=1
+        return "".join(cst)
+            
+        
