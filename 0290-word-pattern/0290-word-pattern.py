ec97e3e185154d3dfd6f0a03dec6888f8c8eebab
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-10-14 09:52:17 +0530

    Time: 40 ms (42.77%), Space: 16.1 MB (84.70%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -2,17 +2,14 @@ class Solution:
     def wordPattern(self, p: str, s: str) -> bool:
         arr = s.split(" ")
         if len(arr)!=len(p):
-            print("k",len(arr),len(s))
             return False
         m1 = {}
         m2 = {}
         for k,v in enumerate(arr):
             x,y=v,p[k]
-            #print(k,v,m1,m2)
             if x in m1 and y not in m2:
                 return False
             if x in m1 and m1[x]!=y:
-                #print("b")
                 return False
             if x not in m1 and y in m2:
                 return False
