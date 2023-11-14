40068dd7756ee2295024e567729d25a2c6c9bd31
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-11-14 10:57:35 +0530

    Time: 53 ms (55.15%), Space: 16.4 MB (48.34%) - LeetHub

--- 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
@@ -1,36 +1,21 @@
-from collections import defaultdict
-class Solution(object):
-    def equationsPossible(self, equations):
-        """
-        :type equations: List[str]
-        :rtype: bool
-        """
-        def index(ch):
-            return ord(ch)-ord('a')
-        self.g = defaultdict(list)
-        
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        par = list(range(26))
+        def find(x):
+            if x!=par[x]:
+                x = find(par[x])
+            return x
+        def union(x,y):
+            a,b = find(x),find(y)
+            par[a]=b
+        av = ord('a')
         for e in equations:
-            if e[1] == "=":
-                a,b = index(e[0]),index(e[-1])
-                self.g[a].append(b)
-                self.g[b].append(a)
-            
-        color = [-1]*26
-        
-        def dfs(node,c):
-            if color[node]==-1:
-                color[node]=c
-                for nei in self.g[node]:
-                    dfs(nei,c)
-                    
-        for i in range(26):
-            if color[i]==-1:
-                dfs(i,i)
-                
+            if e[1]=="=":
+                x,y = ord(e[0])-av,ord(e[3])-av
+                union(x,y)
         for e in equations:
             if e[1]=="!":
-                a,b = index(e[0]),index(e[-1])
-                if color[a]==color[b]:
-                    return False
+                x,y = ord(e[0])-av,ord(e[3])-av
+                if find(x) == find(y):
+                    return False    
         return True
-                
