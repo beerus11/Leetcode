182955a1284392319688b7c3d2dc264ad2632a45
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-11-25 09:17:10 +0530

    Time: 2088 ms (8.08%), Space: 16.7 MB (48.14%) - LeetHub

--- 0939-minimum-area-rectangle/0939-minimum-area-rectangle.py
+++ 0939-minimum-area-rectangle/0939-minimum-area-rectangle.py
@@ -1,15 +1,12 @@
-class Solution(object):
-    def minAreaRect(self, points):
-        """
-        :type points: List[List[int]]
-        :rtype: int
-        """
-        S = set(map(tuple,points))
+class Solution:
+    def minAreaRect(self, points: List[List[int]]) -> int:
+        s = set(map(tuple,points))
         ans = float('inf')
-        for j,p2 in enumerate(points):
-            for i in range(j):
-                p1 = points[i]
-                if (p1[0]!=p2[0] and p1[1]!=p2[1] and (p1[0],p2[1]) in S and (p2[0],p1[1]) in S):
-                    ans = min(ans,abs(p1[0]-p2[0])*abs(p1[1]-p2[1]))
-        return ans if ans!=float('inf') else 0
+        for a,b in s:
+            for x,y in s:
+                if a!=x and b!=y and (x,b) in s and (a,y) in s:
+                    ans = min(ans,abs(x-a)*abs(y-b))
+        return ans if ans<float('inf') else 0
+        
+        
         
