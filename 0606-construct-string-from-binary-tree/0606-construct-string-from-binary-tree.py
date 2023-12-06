f5a7904f741e567681af2effab6aaea2d65fea74
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-12-06 18:30:47 +0530

    Time: 65 ms (6.35%), Space: 18.9 MB (30.67%) - LeetHub

--- 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.py
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.py
@@ -4,28 +4,6 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-'''
- # handle case 1
-        if root is None:
-            return ''
-        # we convert root.val to string here, then append results from different cases
-        s = str(root.val)
-        # handle case 2
-        # this line is obviously not necessary
-        if root.left is None and root.right is None:
-            s += ''
-        # handle case 3
-        if root.left:
-            s += '({})'.format(self.tree2str(root.left))
-        # handle case 4
-        # alternatively, you can use `elif root.right: s += '()'`
-        if root.left is None and root.right:
-            s += '()'
-        # handle case 5
-        if root.right:
-            s += '({})'.format(self.tree2str(root.right))
-        return s    
-'''
 class Solution:
     def tree2str(self, root: Optional[TreeNode]) -> str:
         if root==None:
