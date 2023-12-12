6d24c82079f0178bc51617a54e9a9765c757c107
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-12-12 17:19:57 +0530

    Time: 463 ms (51.08%), Space: 37.7 MB (62.03%) - LeetHub

---
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canMeasureWater(self, jug1Capacity: int, jug2Capacity: int, targetCapacity: int) -> bool:
+        edges = [jug1Capacity,jug2Capacity,-jug1Capacity,-jug2Capacity]
+        q = [0]
+        total = 0
+        seen = set()
+        while q:
+            node = q.pop(0)
+            for e in edges:
+                x = node+e
+                if x==targetCapacity:
+                    return True
+                if x not in seen and x>=0 and x<=jug1Capacity+jug2Capacity:
+                    q.append(x)
+                    seen.add(x)
+        return False
+                
+        
