ec1023a10eecddff4b0b541be0ed1e78f539bdd9
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-12-13 16:41:25 +0530

    Time: 207 ms (11.07%), Space: 16.6 MB (81.43%) - LeetHub

---
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minDays(self, n: int) -> int:
+        q = [(0,n)]
+        seen = set()
+        while q:
+            s,o = heapq.heappop(q)
+            if o<0 or o in seen:
+                continue
+            seen.add(o)
+            if o==0:
+                return s
+            if o%3==0:
+                x = (o-2*(o//3))
+                heapq.heappush(q,(s+1,x))
+            if o%2==0:
+                x = o//2
+                heapq.heappush(q,(s+1,o-x))
+            heapq.heappush(q,(s+1,o-1))
+        
+        return mn
+        
