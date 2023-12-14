59e68aac6f2d6c0cd17e3f9c5d2219c337b96725
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-12-14 15:03:49 +0530

    Time: 67 ms (21.61%), Space: 18.8 MB (7.68%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,11 +1,9 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        if len(cost)==2:
-            return min(cost)
         @lru_cache(None)
         def mc(i):
-            if i<=1:
-                return 0
-            return min(cost[i-1]+mc(i-1),cost[i-2]+mc(i-2))
-        return mc(len(cost))
+            if i>=len(cost)-2:
+                return cost[i]
+            return cost[i]+min(mc(i+1),mc(i+2))
+        return min(mc(0),mc(1))
                 
