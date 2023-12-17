0e1ae2201666242f9c4fc9e2bb4a0ccc39ae5280
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-12-17 12:45:38 +0530

    Time: 69 ms (7.28%), Space: 16.8 MB (14.57%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,27 +1,30 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        count=0
         q = []
+        to=0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j]==1:
-                    count+=1
+                    to+=1
                 elif grid[i][j]==2:
+                    to+=1
                     q.append((i,j,0))
-                    count+=1
-                    
-        dirs = [(0,1),(1,0),(0,-1),(-1,0)]
-        t = 0
+        if to==0:
+            return 0
+        count = 0
+        mxt = -1
         while q:
             i,j,t = q.pop(0)
-            count-=1
-            for a,b in dirs:
-                x,y = a+i,b+j
-                if x>=0 and y>=0 and x<len(grid) and y <len(grid[0]) and grid[x][y]==1:
-                    grid[x][y]=2
+            print(i,j,t)
+            if grid[i][j]==0:
+                continue
+            count+=1
+            mxt = max(mxt,t)
+            grid[i][j]=0
+            for a,b in [(1,0),(0,1),(-1,0),(0,-1)]:
+                x,y = i+a,j+b
+                if x>=0 and y>=0 and x<len(grid) and y<len(grid[0]) and grid[x][y]==1:
                     q.append((x,y,t+1))
-                    
-                    
-        return  t if count==0 else -1
-                    
-            
+        if count ==to:
+            return mxt
+        return -1
