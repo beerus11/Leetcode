9a52e4ab186760994be5f053562243c7f964a326
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-12-17 10:03:23 +0530

    Update 0542-01-matrix.py

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,24 +1,19 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        dp = [[float('inf')]*len(mat[0]) for j in range(len(mat))]
+        q = []
+        seen = set()
+        ans = [[float('inf')]*len(mat[0]) for i in range(len(mat))]
         for i in range(len(mat)):
             for j in range(len(mat[0])):
-                if mat[i][j]==1:
-                    if j>0:
-                        dp[i][j] = min(dp[i][j],dp[i][j-1]+1)
-                    if i>0:
-                        dp[i][j] = min(dp[i-1][j]+1,dp[i][j])
-                else:
-                    dp[i][j] = 0
-                    
-        for i in range(len(mat)-1,-1,-1):
-            for j in range(len(mat[0])-1,-1,-1):
-                if mat[i][j]==1:
-                    if j<len(mat[0])-1:
-                        dp[i][j] = min(dp[i][j+1]+1,dp[i][j])
-                    if i<len(mat)-1:
-                        dp[i][j] = min(dp[i+1][j]+1,dp[i][j])
-                else:
-                    dp[i][j] = 0
-        return dp
-            
+                if mat[i][j]==0:
+                    q.append((i,j,0))
+                    seen.add((i,j))
+        while q:
+            x,y,d = q.pop(0)
+            ans[x][y]=d
+            for i,j in [(0,1),(1,0),(0,-1),(-1,0)]:
+                a,b=x+i,y+j
+                if a>=0 and b>=0 and a<len(mat) and b<len(mat[0]) and (a,b) not in seen:
+                    q.append((a,b,d+1))
+                    seen.add((a,b))
+        return ans
