e4816688755e0d91f89c1c9803e3c7dc1ebc0b29
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-12-18 09:07:24 +0530

    Time: 512 ms (38.56%), Space: 37.6 MB (73.30%) - LeetHub

---
+++ 1032-stream-of-characters/1032-stream-of-characters.py
@@ -0,0 +1,32 @@
+class StreamChecker:
+
+    def __init__(self, words: List[str]):
+        self.trie = {}
+        self.stream = deque([])
+        
+        for word in set(words):
+            node = self.trie
+            for ch in word[::-1]:
+                if not ch in node:
+                    node[ch]={}
+                node = node[ch]
+            node['$'] = word
+        
+
+    def query(self, letter: str) -> bool:
+        self.stream.appendleft(letter)
+        node = self.trie
+        for ch in self.stream:
+            print(ch)
+            if '$' in node:
+                return True
+            if not ch in node:
+                return False
+            node = node[ch]
+        return '$' in node
+        
+
+
+# Your StreamChecker object will be instantiated and called as such:
+# obj = StreamChecker(words)
+# param_1 = obj.query(letter)
