7a1c7aadce17276131010b2d436dd5cda7423dce
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-12-26 13:15:01 +0530

    Time: 52 ms (5.75%), Space: 17.3 MB (5.38%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.py
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -1,39 +1,37 @@
 from collections import defaultdict
 class Solution:
     def alienOrder(self, words: List[str]) -> str:   
-        indegree = {c : 0 for word in words for c in word}
         g = defaultdict(list)
-        
-        for i in range(len(words)-1):
-            for a,b in list(zip(words[i],words[i+1])):
-                if a!=b:
-                    g[a].append(b)
-                    indegree[b]+=1
-                    break
-            else:
-                if len(words[i+1]) < len(words[i]): return ""
-                
-                    
+        indegree = {ch:0 for word in words for ch in word}
+        for a,b in zip(words,words[1:]):
+            if a==b:
+                continue
+            for i in range(len(words)-1):
+                for a,b in list(zip(words[i],words[i+1])):
+                    if a!=b:
+                        g[a].append(b)
+                        indegree[b]+=1
+                        break
+                else:
+                    if len(words[i+1]) < len(words[i]): return ""
+        print(g)    
         q = []
         for k,v in indegree.items():
-            if v ==0:
+            if v==0:
                 q.append(k)
-                
-        if len(q)==0 and len(indegree)==1:
-            return words[0][0]
-
-        order = []
+        ans = []
         while q:
             n = q.pop(0)
-            order.append(n)
+            ans.append(n)
             for nei in g[n]:
                 indegree[nei]-=1
                 if indegree[nei]==0:
                     q.append(nei)
-        if len(indegree)> len(order):
+        if len(ans)<len(indegree):
             return ""
-                    
-        return "".join(order)
+        return "".join(ans)
+            
         
+      
                     
         
