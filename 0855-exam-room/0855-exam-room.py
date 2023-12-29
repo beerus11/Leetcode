4ff07dbe5cadf7574002fb2ca7041abc83557438
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2023-12-29 17:35:12 +0530

    Time: 2504 ms (54.17%), Space: 20.5 MB (37.96%) - LeetHub

---
+++ 0855-exam-room/0855-exam-room.py
@@ -0,0 +1,33 @@
+from sortedcontainers import SortedList
+class ExamRoom:
+
+    def __init__(self, n: int):
+        self.s = SortedList()
+        self.n = n
+        
+
+    def seat(self) -> int:
+        if len(self.s)==0:
+            self.s.add(0)
+            return 0
+        idx = 0
+        diff = self.s[0]
+        for a,b in zip(self.s,self.s[1:]):
+            if (b-a)//2>diff:
+                diff = (b-a)//2
+                idx = a+ (b-a)//2
+        if self.n-1 - self.s[-1] > diff:
+            diff = self.n-1 - self.s[-1]
+            idx = self.n-1
+        self.s.add(idx)
+        return idx
+
+    def leave(self, p: int) -> None:
+        self.s.remove(p)
+        
+
+
+# Your ExamRoom object will be instantiated and called as such:
+# obj = ExamRoom(n)
+# param_1 = obj.seat()
+# obj.leave(p)
