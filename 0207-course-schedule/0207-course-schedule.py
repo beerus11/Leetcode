ff0871038be4e27dde777541cb822538d8db690e
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-01-01 08:59:43 +0530

    Time: 91 ms (88.23%), Space: 19 MB (62.95%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -2,23 +2,20 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         g = defaultdict(list)
         indegree = {i:0 for i in range(numCourses)}
-        
         for a,b in prerequisites:
             g[b].append(a)
             indegree[a]+=1
             
-        q = []
-        for k,v in indegree.items():
-            if v==0:
-                q.append(k)
-        count = 0
+        q = [k for k,v in indegree.items() if v==0]
+        seen = set()
         while q:
-            n = q.pop()
-            count+=1
+            n = q.pop(0)
+            seen.add(n)
             for nei in g[n]:
                 indegree[nei]-=1
                 if indegree[nei]==0:
                     q.append(nei)
-        return count==numCourses
-                
+        return len(seen)==numCourses
+            
+        
         
