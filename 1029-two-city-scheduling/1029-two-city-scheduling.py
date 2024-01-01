28aed3d8079538fae5ca476c9f52327f49a5c107
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-01-01 11:21:49 +0530

    Time: 90 ms (5.01%), Space: 17.3 MB (11.84%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,16 +1,9 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        @lru_cache(None)
-        def mc(i,a,b):
-            if a==0 and b==0:
-                return 0
-            if i==len(costs):
-                return 0
-            x,y=float('inf'),float('inf')
-            if a>0:
-                x = costs[i][0]+mc(i+1,a-1,b)
-            if b>0:
-                y = costs[i][1]+mc(i+1,a,b-1)
-            return min(x,y)
-        return mc(0,len(costs)//2,len(costs)//2)
+        costs.sort(key = lambda x : x[0] - x[1])
         
+        total = 0
+        n = len(costs)//2
+        for i in range(n):
+            total += costs[i][0]+costs[i+n][1]
+        return total
