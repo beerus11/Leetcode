60ca9d133b0ad2f919643dfd472bd394fb30fa5f
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-01-04 08:35:42 +0530

    Time: 535 ms (98.18%), Space: 31.5 MB (18.18%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.py
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.py
@@ -1,20 +1,15 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        nums.sort()
-        @lru_cache
-        def mo(i,s):
-            if s==0:
-                return 0
-            if i==len(nums)-1:
-                return float('inf')
-            a,b = float('inf'),float('inf')
-            if i+2<=len(nums)-1 and nums[i]==nums[i+1]==nums[i+2]:
-                a = 1+mo(i+3,s-(3*nums[i]))
-            if i+1<=len(nums)-1 and nums[i]==nums[i+1]:
-                b = 1+mo(i+2,s-(2*nums[i]))
-            return min(a,b)
-        ans = mo(0,sum(nums))
-        if ans == float('inf'):
-            return -1
+        hm = Counter(nums)
+        ans = 0
+        for k,v in hm.items():
+            if v==1:
+                return -1
+            elif v%3==0:
+                ans+=v//3
+            elif v%3==2:
+                ans+= v//3 +1
+            elif v%3==1:
+                ans+=  (v - 3) // 3 + 2
         return ans
         
