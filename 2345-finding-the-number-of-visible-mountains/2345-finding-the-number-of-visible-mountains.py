17301dfc6dbe519b79f1e1b1f20f0f930c5d1b91
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-01-07 15:30:51 +0530

    Time: 1528 ms (86.28%), Space: 63.2 MB (63.27%) - LeetHub

--- 2345-finding-the-number-of-visible-mountains/2345-finding-the-number-of-visible-mountains.py
+++ 2345-finding-the-number-of-visible-mountains/2345-finding-the-number-of-visible-mountains.py
@@ -1,19 +1,3 @@
-'''
- def visibleMountains(self, peaks: List[List[int]]) -> int:
-        intervals = []
-        for x, y in peaks:
-            intervals.append((x - y, x + y))
-        intervals.sort(key=lambda i: (i[0], -i[1]))
-
-        ans = 0
-        max_end = -inf
-        for i in range(len(intervals)):
-            start, end = intervals[i]
-            next_is_same = (i < len(intervals) - 1 and intervals[i+1] == intervals[i])
-            ans += (end > max_end and not next_is_same)
-            max_end = max(end, max_end)
-        return ans
-'''
 class Solution:
     def visibleMountains(self, peaks: List[List[int]]) -> int:
         arr = [(a-b,a+b) for a,b in peaks]
