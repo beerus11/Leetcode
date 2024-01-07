22a9533a351b2e7820be40d61bd27664180dae38
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-01-07 14:59:06 +0530

    Time: 265 ms (81.05%), Space: 19.4 MB (5.26%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.py
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.py
@@ -1,25 +1,3 @@
-"""
-class Solution:
-    def maximumGain(self, s: str, x: int, y: int) -> int:
-        a, b = 'ab', 'ba'
-        if y > x: 
-            x, y = y, x
-            a, b = 'ba', 'ab'
-        stack1 = []
-        ans = 0
-        for le in s:
-            if stack1 and stack1[-1] == a[0] and le == a[1]: 
-                stack1.pop()
-                ans += x
-            else: stack1.append(le)
-        stack2 = []
-        for le in stack1:
-            if stack2 and stack2[-1] == b[0] and le == b[1]: 
-                stack2.pop()
-                ans += y
-            else: stack2.append(le)
-        return ans
-"""
 class Solution:
     def maximumGain(self, s: str, x: int, y: int) -> int:
         a, b = 'ab', 'ba'
