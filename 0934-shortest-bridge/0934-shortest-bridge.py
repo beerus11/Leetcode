4f517dc2632847dc5f89764e50d7eeca1317d8da
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-01-17 20:32:05 +0530

    Time: 293 ms (85.95%), Space: 19.2 MB (56.01%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.py
+++ 0934-shortest-bridge/0934-shortest-bridge.py
@@ -1,36 +1,40 @@
 from collections import deque
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
-        self.q = deque()
-        vis = set()
-        def dfs(i,j,no):
-            if i<0 or j<0 or i>=len(grid) or j>=len(grid) or grid[i][j]==0 or (i,j) in vis:
+        q = deque([])
+        def dfs(i,j):
+            if grid[i][j]==0:
                 return
-            vis.add((i,j))
-            grid[i][j]=no
-            self.q.append((i,j,0))
-            dfs(i+1,j,no)
-            dfs(i,j+1,no)
-            dfs(i-1,j,no)
-            dfs(i,j-1,no)
-        
-        def mark_one():
-            for i in range(len(grid)):
-                for j in range(len(grid)):
-                    if grid[i][j]==1:
-                        dfs(i,j,0)
-                        return
-        mark_one()
-        q = self.q
+            grid[i][j]=0
+            q.append((i,j,0))
+            for a,b in [(1,0),(0,1),(-1,0),(0,-1)]:
+                x,y = i+a,j+b
+                if x>=0 and y>=0 and x<len(grid) and y<len(grid[0]) and grid[x][y]==1:
+                    dfs(x,y)
+        run = True            
+        for i in range(len(grid)):
+            if not run:
+                break
+            for j in range(len(grid[0])):
+                if grid[i][j]==1:
+                    dfs(i,j)
+                    run = False
+                    break
+                    
         while q:
-            i,j,l = q.popleft()
-            for a,b in [[1,0],[0,1],[-1,0],[0,-1]]:
-                x,y=a+i,b+j
-                if x>=0 and y>=0 and x<len(grid) and y<len(grid):
+            i,j,k = q.popleft()
+            if grid[i][j]==1:
+                return k-1
+            for a,b in [(1,0),(0,1),(-1,0),(0,-1)]:
+                x,y = i+a,j+b
+                if x>=0 and y>=0 and x<len(grid) and y<len(grid[0]):
                     if grid[x][y]==1:
-                        return l
-                    elif grid[x][y]==0 :
-                        q.append((x,y,l+1))
+                        return k
+                    elif grid[x][y]==0:
+                        q.append((x,y,k+1))
                         grid[x][y]=-1
         return -1
+            
+            
+        
         
