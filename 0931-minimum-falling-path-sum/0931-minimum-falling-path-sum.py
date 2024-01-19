1ad561776a9d44b766119fea70febe4fcffdeca4
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-01-19 06:44:45 +0530

    Time: 143 ms (41.64%), Space: 27 MB (5.08%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -1,18 +1,22 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        self.dp = {}
-        def dfs(i,j):
-            if (i,j) in self.dp:
-                return self.dp[(i,j)]
-            if j<0 or j>=len(matrix):
-                self.dp[(i,j)] = float('inf')
-                return self.dp[(i,j)]
-            if i==len(matrix)-1:
-                self.dp[(i,j)]= matrix[i][j]
-                return self.dp[(i,j)]
-            a = dfs(i+1,j)     
-            b = dfs(i+1,j-1)
-            c = dfs(i+1,j+1)
-            self.dp[(i,j)] = min([a,b,c])+matrix[i][j]
-            return self.dp[(i,j)]
-        return min([dfs(0,i) for i in range(len(matrix))])
+        n = len(matrix)
+        @lru_cache(None)
+        def ms(i,j):
+            if i==n:
+                return 0
+            if j==n or j==-1:
+                return float('inf')
+            a = ms(i+1,j-1)
+            b = ms(i+1,j)
+            c = ms(i+1,j+1)
+            return matrix[i][j]+min(a,b,c)
+        ans = float('inf')
+        for i in range(len(matrix)):
+            a = ms(1,i-1)
+            b = ms(1,i)
+            c = ms(1,i+1)
+            ans = min(ans,matrix[0][i]+min([a,b,c]))
+        return ans
+            
+        
