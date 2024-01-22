0389b18fe58b31e3b41d0d8b02c7706fb4924e15
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-01-22 08:53:53 +0530

    Time: 55 ms (76.96%), Space: 17.1 MB (65.44%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,21 +1,21 @@
-class DSU(object):
-    def __init__(self):
-        self.par = list(range(1001))
-    
-    def find(self,x):
-        if self.par[x]!=x:
-            self.par[x] = self.find(self.par[x])
-        return self.par[x]
-    def union(self,x,y):
-        xr,yr = self.find(x),self.find(y)
-        if xr == yr:
-            return False
-        self.par[self.find(x)] = self.find(y)
-        return True
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        dsu = DSU()
-        for edge in edges:
-            if not dsu.union(*edge):
-                return edge
+        parents = list(range(len(edges)+1))
+        def find(x):
+            if x!=parents[x]:
+                return find(parents[x])
+            return x
+        def union(x,y):
+            a = find(x)
+            b = find(y)
+            if a==b:
+                return False
+            parents[a]=b
+            return True
+        for a,b in edges:
+            if not union(a,b):
+                return [a,b]
+        return []
+                
+            
         
