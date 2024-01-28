9e74342b736e7b573fcb1392ea6ad3a6e95aa965
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-01-28 19:28:13 +0530

    Time: 34 ms (80.22%), Space: 17 MB (55.75%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,16 +1,11 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        self.dp = {}
+        @lru_cache(None)
         def dfs(i,j):
-            if (i,j) in self.dp:
-                return self.dp[(i,j)]
-            if i>=m or j>=n:
-                return 0
-            if i==m-1 and j ==n-1:
+            if i==m-1 and j==n-1:
                 return 1
-            self.dp[(i,j)] = dfs(i+1,j) + dfs(i,j+1)
-            return self.dp[(i,j)]
+            if i==m or j==n:
+                return 0
+            return dfs(i+1,j)+dfs(i,j+1)
         return dfs(0,0)
-
-            
         
