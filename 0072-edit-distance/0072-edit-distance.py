b669dee4b5a000d0e38b47aae05c3dcfbb86e981
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-01-28 20:20:24 +0530

    Time: 78 ms (91.80%), Space: 19.1 MB (84.86%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,16 +1,19 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         @lru_cache(None)
-        def d(i,j):
+        def mo(i,j):
+            if i==len(word1) and j==len(word2):
+                return 0
             if i==len(word1):
                 return len(word2)-j
             if j==len(word2):
                 return len(word1)-i
             if word1[i]==word2[j]:
-                return d(i+1,j+1)
-            else:
-                a=d(i+1,j)
-                b=d(i,j+1)
-                c=d(i+1,j+1)
-                return min([a,b,c])+1
-        return d(0,0)
+                return mo(i+1,j+1)
+            a = 1+mo(i,j+1)
+            b = 1+mo(i+1,j)
+            c = 1+mo(i+1,j+1)
+            return min([a,b,c])
+        return mo(0,0)
+        
+        
