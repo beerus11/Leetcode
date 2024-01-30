77c3109e34846f279624b9d973bb4e227169fe09
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-01-30 15:37:39 +0530

    Time: 49 ms (60.41%), Space: 16.7 MB (62.04%) - LeetHub

--- 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
@@ -1,21 +1,41 @@
+class DSU:
+    def __init__(self):
+        self.parent = list(range(26))
+        
+    def find(self,x):
+        if x!=self.parent[x]:
+            return self.find(self.parent[x])
+        return x
+        
+    def union(self,a,b):
+        x = self.find(a)
+        y = self.find(b)
+        if x==y:
+            return False
+        self.parent[x]=y
+        return True
+        
 class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
-        par = list(range(26))
-        def find(x):
-            if x!=par[x]:
-                x = find(par[x])
-            return x
-        def union(x,y):
-            a,b = find(x),find(y)
-            par[a]=b
-        av = ord('a')
-        for e in equations:
-            if e[1]=="=":
-                x,y = ord(e[0])-av,ord(e[3])-av
-                union(x,y)
-        for e in equations:
-            if e[1]=="!":
-                x,y = ord(e[0])-av,ord(e[3])-av
-                if find(x) == find(y):
-                    return False    
+        dsu = DSU()
+        for eq in equations:
+            if eq.find("!=")==-1:
+                a = ord(eq[0])-97
+                b = ord(eq[-1])-97
+                if a!=b:
+                    dsu.union(a,b)
+        for eq in equations:
+            if eq.find("!=")!=-1:
+                a = ord(eq[0])-97
+                b = ord(eq[-1])-97
+                if a==b:
+                    return False
+                
+                para = dsu.find(a)
+                parb = dsu.find(b)
+                if para==parb:
+                    return False
         return True
+                    
+                
+        
