44a48b7aa0c85e4e5f5edb85142d00720a3db5b4
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-02-03 21:27:03 +0530

    Time: 34 ms (76.92%), Space: 16.5 MB (64.96%) - LeetHub

--- 1180-count-substrings-with-only-one-distinct-letter/1180-count-substrings-with-only-one-distinct-letter.py
+++ 1180-count-substrings-with-only-one-distinct-letter/1180-count-substrings-with-only-one-distinct-letter.py
@@ -1,12 +1,15 @@
 class Solution:
     def countLetters(self, s: str) -> int:
         count = 0
-        for i in range(len(s)):
-            st = s[i]
-            count+=1
-            for j in range(i+1,len(s)):
-                if st!=s[j]:
-                    break
-                count+=1
+        left = 0
+        for i in range(1,len(s)):
+            if s[i]!=s[i-1]:
+                n = i-left
+                count += n*(n+1)//2
+                left = i
+        if left<len(s):
+            n = len(s)-left
+            count += n*(n+1)//2
+            
         return count
         
