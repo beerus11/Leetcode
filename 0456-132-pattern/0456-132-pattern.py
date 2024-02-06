18af5d0784488d00bc76ab03d4848394d2e05b7a
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-02-06 13:43:41 +0530

    Time: 540 ms (70.02%), Space: 37 MB (54.41%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -2,23 +2,16 @@ class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
         if len(nums)<3:
             return False
-        
-        min_arr = []
-        for i in range(len(nums)):
-            if len(min_arr)==0:
-                min_arr.append(nums[i])
-            else:
-                min_arr.append(min(min_arr[-1],nums[i]))
                 
         st =[]
-        for i in range(len(nums)-1,-1,-1):
-            if min_arr[i-1]>=nums[i]:
-                continue
-            while st and min_arr[i]>=st[-1]:
+        min_curr = nums[0]
+        for i in nums[1:]:
+            while st and st[-1][0]<=i:
                 st.pop()
-            if st and st[-1]<nums[i]:
+            if st and st[-1][1]<i:
                 return True
-            st.append(nums[i])
+            st.append((i,min_curr))
+            min_curr =min(min_curr,i)
         return False
                 
         
