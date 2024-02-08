7c3b17bd23f2db8e7282f7132925cd32e8a75ddb
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-02-08 16:56:08 +0530

    Time: 1080 ms (15.76%), Space: 230.1 MB (14.25%) - LeetHub

--- 1062-longest-repeating-substring/1062-longest-repeating-substring.py
+++ 1062-longest-repeating-substring/1062-longest-repeating-substring.py
@@ -1,11 +1,17 @@
 class Solution:
     def longestRepeatingSubstring(self, s: str) -> int:
         hm = defaultdict(int)
-        ls,l = "",0
         for i in range(len(s)):
-            for j in range(len(s)):
+            for j in range(i,len(s)):
                 hm[s[i:j+1]]+=1
-                if hm[s[i:j+1]]>1:
-                    l = max(l,len(s[i:j+1]))
-        return l
+        mx=1
+        ans = ""
+        for k,v in hm.items():
+            if v>1 and len(k)>mx:
+                mx = len(k)
+                ans = k
+        if mx==1:
+            return 0
+        return len(ans)
+                
         
