9dfdba3f1f3c4b1bf942a82fd3953714a27c175c
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-02-09 05:55:13 +0530

    Time: 831 ms (49.68%), Space: 19.8 MB (75.00%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -1,37 +1,27 @@
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
-
-        indegree = {}
-        ss = set(supplies)
-        rs = set(recipes)
         
-        for r in recipes:
-            indegree[r]=0
-            
-        for k,items in enumerate(ingredients):
-            for item in items:
-                indegree[item]=0
-        
+        recipes_set = set(recipes)
         g = defaultdict(list)
-        for k,items in enumerate(ingredients):
-            for item in items:
-                g[item].append(recipes[k])
-                indegree[recipes[k]]+=1
-                
-        q = [k for k,v in indegree.items() if v==0 and k in ss]
-        # print(q)
-        # print(indegree)
-        # print(g)
         
-        ans = []
+        indegree = defaultdict(int)
+        for i in range(len(ingredients)):
+            for j in range(len(ingredients[i])):
+                item = ingredients[i][j]
+                g[item].append(recipes[i])
+                indegree[recipes[i]]+=1
+                
+        q = deque(supplies)
+        ans = set()
+        
         while q:
-            node = q.pop(0)
-            if node in rs:
-                ans.append(node)
+            node = q.popleft()
+            if node in recipes_set:
+                ans.add(node)
             for nei in g[node]:
                 indegree[nei]-=1
                 if indegree[nei]==0:
                     q.append(nei)
-        #print(indegree)
         return ans
+                
         
