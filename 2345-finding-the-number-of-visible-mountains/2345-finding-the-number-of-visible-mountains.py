e5112d3b850fe3126702e4238777e5fe95582217
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-02-11 12:58:59 +0530

    Time: 1501 ms (73.77%), Space: 63.3 MB (61.89%) - LeetHub

--- 2345-finding-the-number-of-visible-mountains/2345-finding-the-number-of-visible-mountains.py
+++ 2345-finding-the-number-of-visible-mountains/2345-finding-the-number-of-visible-mountains.py
@@ -1,17 +1,24 @@
 class Solution:
     def visibleMountains(self, peaks: List[List[int]]) -> int:
+        if len(peaks)==1:
+            return 1
         arr = [(a-b,a+b) for a,b in peaks]
-        arr.sort(key=lambda i: (i[0], -i[1]))
-        le =0
-        count = 0
-        for i in range(len(arr)):
-            a,b = arr[i]
-            next_is_same = False
-            if i < len(arr) - 1 and arr[i+1] == arr[i]:
-                next_is_same=True
-            if le<b and not next_is_same:
-                count+=1
-            le = max(le,b)
-        return count
-        
+        arr.sort(key =lambda x:(x[0],-1*x[1]))
+        st = [arr[0]]
+        i = 1
+        duplicate_count = 0
+        while i <len(arr):
+            duplicate = False
+            while st and i<len(arr) and arr[i]==st[-1]:
+                duplicate = True
+                i+=1
+            if duplicate and st:
+                duplicate_count+=1
+            if i==len(arr):
+                break
+            if len(st)==0 or arr[i][1]>st[-1][1]:
+                st.append(arr[i])
+            i+=1
+        return len(st)-duplicate_count
+            
         
