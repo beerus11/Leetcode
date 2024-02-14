aa0963621d2673f13eff414fc7b41baca33eb0a5
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-02-14 08:24:48 +0530

    Time: 104 ms (78.70%), Space: 17.7 MB (63.72%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,12 +1,13 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        @lru_cache(None)
-        def solve(i):
-            if i>=len(nums)-1:
-                return 0
-            mn = float('inf')
-            for j in range(1,nums[i]+1):
-                mn = min(mn,solve(i+j))
-            return 1+mn
-        return solve(0)
+        cur_far = 0
+        cur_end = 0
+        ans = 0 
+        for i in range(len(nums)-1):
+            cur_far = max(cur_far,nums[i]+i)
+            if i== cur_end:
+                ans +=1
+                cur_end = cur_far
+        return ans
+            
         
