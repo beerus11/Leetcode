10f93d0ede566a1edc59d801f5095c1e8a929ece
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-02-14 07:40:36 +0530

    Time: 4533 ms (14.21%), Space: 32.1 MB (5.01%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,15 +1,12 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         @lru_cache(None)
-        def reach(i):
-            if i>len(nums)-1:
-                return float('inf')
-            if i== len(nums)-1:
+        def solve(i):
+            if i>=len(nums)-1:
                 return 0
             mn = float('inf')
             for j in range(1,nums[i]+1):
-                mn = min(mn,1+reach(i+j))
-            return mn
-        return reach(0)
-                
+                mn = min(mn,solve(i+j))
+            return 1+mn
+        return solve(0)
         
