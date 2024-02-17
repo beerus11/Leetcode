4182ba35a3caaff8f5d02f31ec0e4ac53ff724aa
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-02-17 10:47:51 +0530

    Time: 89 ms (96.81%), Space: 17 MB (61.65%) - LeetHub

--- 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.py
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.py
@@ -1,29 +1,48 @@
 class DSU:
     def __init__(self,n):
-        self.par = list(range(n+1))
+        self.parent = list(range(n))
+        self.rank = [1]*n
         
-    def find(self,x):
-        if self.par[x]!=x:
-            self.par[x] = self.find(self.par[x])
-        return self.par[x]
-            
     def union(self,a,b):
-        xr,yr = self.find(a),self.find(b)
-        if xr==yr:
+        par_a = self.find(a)
+        par_b = self.find(b)
+        if par_a==par_b:
             return False
-        self.par[xr] = yr
+        
+        rank_a = self.parent[a]
+        rank_b = self.parent[b]
+        
+        if rank_a>rank_b:
+            self.parent[par_b]=par_a
+            self.rank[par_a]+=1
+        else:
+            self.parent[par_a]=par_b
+            self.rank[par_b]+=1
+        
         return True
+    
+    def find(self,a):
+        if self.parent[a]!=a:
+            return self.find(self.parent[a])
+        return a
+    
 class Solution:
     def earliestAcq(self, logs: List[List[int]], n: int) -> int:
-        logs.sort(key= lambda x:x[0])
-        ds = DSU(n)
+        logs.sort()
+        
+        dsu = DSU(n) 
         count = n
+        
         for t,a,b in logs:
-            if ds.union(a,b):
-                count -=1
-            if count == 1:
+            if dsu.union(a,b):
+                count-=1
+            
+            if count ==1:
                 return t
+                
         return -1
             
+                
+                
             
         
