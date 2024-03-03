f11b9dfa69e67ac689d1cbe3e86291bc3076bf9d
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-03-03 12:35:43 +0530

    Time: 2559 ms (46.15%), Space: 20 MB (67.69%) - LeetHub

--- 0855-exam-room/0855-exam-room.py
+++ 0855-exam-room/0855-exam-room.py
@@ -5,23 +5,24 @@ class ExamRoom:
         self.s = SortedList()
         self.n = n
         
-
     def seat(self) -> int:
         if len(self.s)==0:
             self.s.add(0)
             return 0
-        idx = 0
+        
         diff = self.s[0]
+        idx = 0
         for a,b in zip(self.s,self.s[1:]):
-            if (b-a)//2>diff:
-                diff = (b-a)//2
-                idx = a+ (b-a)//2
-        if self.n-1 - self.s[-1] > diff:
+            x = (b-a)//2
+            if x>diff:
+                diff=x
+                idx=a+x
+        if (self.n-1)-self.s[-1]>diff:
             diff = self.n-1 - self.s[-1]
-            idx = self.n-1
+            idx= self.n-1
         self.s.add(idx)
         return idx
-
+            
     def leave(self, p: int) -> None:
         self.s.remove(p)
         
