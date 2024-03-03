505565297a26655d48e314a6f96b954725e84edd
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-03-03 10:41:58 +0530

    Time: 38 ms (49.17%), Space: 16.7 MB (68.14%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,27 +1,30 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         g = defaultdict(defaultdict)
-        
-        for k,v in enumerate(equations):
-            g[v[0]][v[1]] = values[k]
-            g[v[1]][v[0]] = 1/values[k]
-        
-        visited = set()
+        for i in range(len(equations)):
+            a,b = equations[i]
+            g[a][b]=values[i]
+            g[b][a]=1/values[i]
+        v = set()
         def dfs(src,dest,no):
             if src==dest:
                 return no
-            visited.add(src)
-            for n,v in g[src].items():
-                if n not in visited:
-                    x = dfs(n,dest,no*v)
+            v.add(src)
+            ans = 1
+            for nei,value in g[src].items():
+                if nei not in v:
+                    x = dfs(nei,dest,no*value)
                     if x!=-1.0:
                         return x
             return -1.0
+        
         ans = []
-        for a,b in queries:
+        for q in queries:
+            a,b = q
+            v = set()
             if a not in g or b not in g:
                 ans.append(-1.0)
             else:
-                visited = set()
                 ans.append(dfs(a,b,1))
         return ans
+        
