c64fe36de0c02f061459c69417c74e820040d007
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-03-03 20:20:24 +0530

    Time: 425 ms (43.52%), Space: 18.7 MB (33.80%) - LeetHub

--- 0737-sentence-similarity-ii/0737-sentence-similarity-ii.py
+++ 0737-sentence-similarity-ii/0737-sentence-similarity-ii.py
@@ -1,47 +1,26 @@
-class DSU:
-    def __init__(self,l):
-        self.par = {}
-        self.rank = {}
-    def add_word(self,w):
-        if w not in self.par:
-            self.par[w]=w
-            self.rank[w]=0
-            
-    def is_present(self,w):
-        return w in self.par
-        
-    def union(self,a,b):
-        a_par = self.find(a)
-        b_par = self.find(b)
-        if a_par==b_par:
-            return
-        elif self.rank[a_par] < self.rank[b_par]:
-            self.par[a_par]=b_par
-        elif self.rank[a_par] > self.rank[b_par]:
-            self.par[b_par]=a_par
-        else:
-            self.par[b_par]=a_par
-            self.rank[a_par]+=1
-    def find(self,a):
-        if self.par[a]!=a:
-            return self.find(self.par[a])
-        return self.par[a]
 class Solution:
     def areSentencesSimilarTwo(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
         if len(sentence1)!=len(sentence2):
             return False
-        dsu = DSU(len)
+        
+        g = defaultdict(list)
         for a,b in similarPairs:
-            dsu.add_word(a)
-            dsu.add_word(b)
-            dsu.union(a,b)
+            g[a].append(b)
+            g[b].append(a)
+        v = set()
+        def dfs(src,dest):
+            if src==dest:
+                return True
+            v.add(src)
+            for nei in g[src]:
+                if nei not in v and dfs(nei,dest):
+                    return True  
+            return False
             
         for i in range(len(sentence1)):
-            if sentence1[i]==sentence2[i]:
-                continue
-            if dsu.is_present(sentence1[i]) and dsu.is_present(sentence2[i]) and dsu.find(sentence1[i]) == dsu.find(sentence2[i]):
-                continue
-            return False
+            v = set()
+            if not dfs(sentence1[i],sentence2[i]):
+                return False
+            
         return True
-        
         
