ccfb938a233e1d9b053ac3e6f93b082879528adc
Anurag Goel <6936363+beerus11@users.noreply.github.com>
2024-03-04 11:54:11 +0530

    Time: 468 ms (96.31%), Space: 22.2 MB (94.83%) - LeetHub

--- 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.py
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.py
@@ -1,42 +1,42 @@
 class DSU:
     def __init__(self,n):
-        self.par = list(range(n+1))
-        self.rank = [1]*(n+1)
+        self.parents = list(range(n+1))
+        self.ranks = [1]*(n+1)
         
-    def find(self,x):
-        if self.par[x]!=x:
-            self.par[x] = self.find(self.par[x])
-        return self.par[x]
-    
-    def union(self,x,y):
-        par1 = self.find(x)
-        par2 = self.find(y)      
-        if self.rank[par1] > self.rank[par2]:
-            self.par[par2] = par1
-            self.rank[par1]+=self.rank[par2]
+    def union(self,a,b):
+        par_a = self.find(a)
+        par_b = self.find(b)
+        if par_a ==par_b:
+            return False
+        
+        if self.ranks[par_a]>self.ranks[par_b]:
+            self.parents[par_b]=par_a
+            self.ranks[par_a]+=self.ranks[par_b]
         else:
-            self.par[par1] = par2
-            self.rank[par2]+=self.rank[par1]
-            
-    def is_same(self,a,b):
-        return self.find(a)==self.find(b)
-            
-            
+            self.parents[par_a]=par_b
+            self.ranks[par_b]+=self.ranks[par_a]
+        return True
+    
+    def find(self,a):
+        if a!=self.parents[a]:
+            return self.find(self.parents[a])
+        return a
 class Solution:
     def minimumCost(self, n: int, connections: List[List[int]]) -> int:
-        connections.sort(key= lambda x: x[2])
-        cost = 0
-        ds = DSU(n)
-        total = 0
-        for i in range(len(connections)):
-            a,b,c = connections[i]
-            if ds.is_same(a,b):
-                continue
-            ds.union(a,b)
-            cost += c
-            total+=1
+        connections.sort(key=lambda x:x[2])
+        dsu = DSU(n)
         
-        if total == n-1:
-            return cost
-        return -1
+        total_cost = 0
+        joins = 0
+        for a,b,c in connections:
+            if dsu.union(a,b):
+                total_cost+=c
+                joins+=1
+        if joins!=n-1:
+            return -1
+                
+        return total_cost
+            
+        
+        
         
